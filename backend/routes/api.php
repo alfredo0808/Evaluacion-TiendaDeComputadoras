5b2093b3dfe59d19f6a3b951a2850a4ea3660e8d
alfredo <alfredoajatavargas@gmail.com>
2025-08-02 23:11:49 -0400

    Models de Marcas y Productos

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\ProductoController;
+
+Route::apiResource('marcas',MarcaController::class);
+Route::apiResource('productos',ProductoController::class);
 
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
-Route::apiResource('marcas',MarcaController::class);
