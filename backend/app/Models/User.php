5fa3f3032b8d6e7376193782cb2c8f6b074eb565
alfredo <alfredoajatavargas@gmail.com>
2025-08-02 23:49:35 -0400

    EVALUACION COMPLETADA

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -9,7 +9,8 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
-{
+{    
+    // EVALUACION TERMINADA :)
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable,HasApiTokens,HasFactory;
 
